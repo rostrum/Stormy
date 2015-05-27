62c8aa672459490b933e753db36b87c15044604e
Gorka Berzal <gorka.berzal@gmail.com>
2015-05-27 18:06:55 +0200

    Using Parcelable Data

--- app/src/main/java/com/teamtreehouse/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/teamtreehouse/stormy/ui/DailyForecastActivity.java
@@ -1,6 +1,8 @@
 package com.teamtreehouse.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,8 @@ import com.teamtreehouse.stormy.R;
 import com.teamtreehouse.stormy.adapters.DayAdapter;
 import com.teamtreehouse.stormy.weather.Day;
 
+import java.util.Arrays;
+
 public class DailyForecastActivity extends ListActivity {
 
     private Day[] mDays;
@@ -20,7 +24,13 @@ public class DailyForecastActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daly_forecast);
 
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
         DayAdapter adapter = new DayAdapter(this, mDays);
+
+        setListAdapter(adapter); // <-- THIS LINE
     }
 
 }
--- app/src/main/java/com/teamtreehouse/stormy/ui/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/stormy/ui/MainActivity.java
@@ -39,6 +39,7 @@ import butterknife.OnClick;
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String DAILY_FORECAST = "DAILY_FORECAST";
 
     private Forecast mForecast;
 
@@ -258,6 +259,7 @@ public class MainActivity extends ActionBarActivity {
     @OnClick (R.id.dailyButton)
     public void startDailyActivity(View view) {
         Intent intent = new Intent(this, DailyForecastActivity.class);
+        intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
         startActivity(intent);
     }
 
